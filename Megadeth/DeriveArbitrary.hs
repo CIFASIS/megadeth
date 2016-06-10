5ee17f2c7c4846bd16e47b862370efecfa098305
Martin A. Ceresa <martinceresa@gmail.com>
2016-06-10 10:28:58 -0300

    fix

--- Megadeth/DeriveArbitrary.hs
+++ Megadeth/DeriveArbitrary.hs
@@ -16,18 +16,20 @@ import Data.DeriveTH
 import Data.Derive.Show
 
 -- | Build the arbitrary function with makeArbs
-chooseExpQ :: Name -> Name -> Integer -> Type -> ExpQ
-chooseExpQ n t bf (AppT ListT ty) = [| listOf $ resize ($(varE  n) `div` 10) arbitrary |]
-chooseExpQ n t bf ty | headOf ty /= t = [| resize $(varE n) arbitrary |]
-chooseExpQ n t bf ty =
+chooseExpQ :: Name -> Name -> Name -> Integer -> Type -> ExpQ
+chooseExpQ g n t bf (AppT ListT ty) = [| listOf $ resize ($(varE  n) `div` 10) arbitrary |]
+chooseExpQ g n t bf ty | headOf ty /= t = [| resize $(varE n) arbitrary |]
+chooseExpQ g n t bf ty =
   case bf of
     0  -> [| arbitrary |] --varE 'arbitrary
-    1  -> appE (varE (mkName "go")) [| ($(varE n) - 1) |]
-    bf -> appE (varE (mkName "go")) [| ($(varE n) `div` bf) |]
+    1  -> [| $(varE g) $ $(varE n) - 1 |]
+--appE (varE (mkName "go")) [| () |]
+    bf -> [| $(varE g) $ $(varE n) `div` bf |]
+--appE (varE (mkName "go")) [|  |]
 
 
-makeArbs :: Name -> Name ->  [ConView] -> [ExpQ]
-makeArbs n t xs = map (fmap fixAppl) [ foldl (\h ty -> uInfixE h (varE '(<*>)) (chooseExpQ n t bf ty)) (conE name) tys' | SimpleCon name bf tys' <- xs]
+makeArbs :: Name -> Name -> Name ->  [ConView] -> [ExpQ]
+makeArbs g n t xs = map (fmap fixAppl) [ foldl (\h ty -> uInfixE h (varE '(<*>)) (chooseExpQ g n t bf ty)) (conE name) tys' | SimpleCon name bf tys' <- xs]
 
 -- | Generic function used to create arbitrarily large tuples
 -- @
@@ -60,22 +62,28 @@ deriveArbitrary t = do
               let ns  = map varT $ paramNames params
                   scons = map (simpleConView t) constructors
                   fcs = filter ((==0) . bf) scons
-                  gos n = if length scons > 1
+                  gos g n = if length scons > 1
                           then
-                                [|  if $(varE n) > 1
-                                    then oneof $(listE (makeArbs n t fcs))
-                                    else oneof $(listE (makeArbs n t scons))|]
+                                if length fcs > 1 
+                                then
+                                    [|  if $(varE n) <= 1
+                                    then oneof $(listE (makeArbs g n t fcs))
+                                    else oneof $(listE (makeArbs g n t scons))|]
+                                else
+                                    [|  if $(varE n) <= 1
+                                    then $(head (makeArbs g n t fcs))
+                                    else oneof $(listE (makeArbs g n t scons))|]
                           else
-                                [| $(head (makeArbs n t scons)) |]
+                                [| $(head (makeArbs g n t scons)) |]
               if not $ null ns then
                [d| instance $(applyTo (tupleT (length ns)) (map (appT (conT ''Arbitrary)) ns))
                             => Arbitrary $(applyTo (conT t) ns) where
                               arbitrary = sized go
-                                where go n = $(gos 'n) |]
+                                where go n = $(gos 'go 'n) |]
                else
                 [d| instance Arbitrary $(applyTo (conT t) ns) where
                                arbitrary = sized go
-                                 where go n = $(gos 'n)|]
+                                 where go n = $(gos 'go 'n)|]
         TyConI (NewtypeD _ _ params con _) -> do 
             let ns = map varT $ paramNames params
                 scon = simpleConView t con
@@ -83,11 +91,11 @@ deriveArbitrary t = do
                [d| instance $(applyTo (tupleT (length ns)) (map (appT (conT ''Arbitrary)) ns))
                             => Arbitrary $(applyTo (conT t) ns) where
                               arbitrary = sized go 
-                                where go n = $(head (makeArbs 'n t [scon])) |]
+                                where go n = $(head (makeArbs 'go 'n t [scon])) |]
                else
                 [d| instance Arbitrary $(applyTo (conT t) ns) where
                                arbitrary = sized go
-                                where go n = $(head (makeArbs 'n t [scon])) |]
+                                where go n = $(head (makeArbs 'go 'n t [scon])) |]
         TyConI inp@(TySynD _ params ty) ->
             case (getTy ty) of
                 (TupleT n) -> 
